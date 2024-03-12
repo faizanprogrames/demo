d6d89d6ecf25205b7c50e1c1b661a453435fe1c4
JavaScript Space Projects <124654425+javascriptspace-projects@users.noreply.github.com>
2024-03-12 06:03:04 +0500

    Initial Commit

--- app/profile/api/route.ts
+++ app/profile/api/route.ts
@@ -1,6 +1,17 @@
-import { headers } from "next/headers";
+import { NextRequest } from "next/server";
+import { cookies, headers } from "next/headers";
 
-export async function GET() {
+export async function GET(request: NextRequest) {
   const headersList = headers();
-  return new Response("Profile Route!");
+  const requestHeaders = new Headers(request.headers);
+  console.log(headersList.get("Authorization"));
+  console.log(requestHeaders.get("Authorization"));
+  cookies().set("resultsPerPage", "20");
+  console.log(cookies().get("resultsPerPage"));
+
+  return new Response("<h1>Profile Route!</h1>", {
+    headers: {
+      "Content-type": "text/html",
+    },
+  });
 }
